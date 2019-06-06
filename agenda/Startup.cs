61297b2a593353deee9a5a98cff00b9e6c45e8f3
vicher <frankvicher@gmail.com>
2019-06-06 09:41:56 -0500

    Se agregó la cadena de conexión y la dependencia del contexto de datos

--- agenda/Startup.cs
+++ agenda/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using agenda.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +30,10 @@ namespace agenda
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<DataContext>(o =>
+                o.UseSqlServer(Configuration.GetConnectionString("agenda"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
